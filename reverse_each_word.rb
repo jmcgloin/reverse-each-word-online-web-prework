3c5fc1e1b4105739682e975cf224c60ba9d5b7be
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 01:21:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   ar = sentence.split(" ")
-  newar = ar.each { |word|  word = word.reverse }
+  newar = ar.each { |word| word.reverse }
   sentence = newar.join(" ")
   sentence
 end
